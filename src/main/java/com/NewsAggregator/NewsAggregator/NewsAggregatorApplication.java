98bcbef3e1e977b569e52232167b35594fbbb675
shreyash0185 <shreyashmuktalkar98@gmail.com>
2025-06-29 10:50:11 +0530

    intial commit

---
+++ src/main/java/com/NewsAggregator/NewsAggregator/NewsAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.NewsAggregator.NewsAggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggregatorApplication.class, args);
+	}
+
+}
